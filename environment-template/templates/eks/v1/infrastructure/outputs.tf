5662a094b86906f93377652a0a23adb3bfb805ae
Shridhar <36041359+shridharMe@users.noreply.github.com>
2022-09-22 19:14:26 +0100

    Update outputs.tf

--- environment-template/templates/eks/v1/infrastructure/outputs.tf
+++ environment-template/templates/eks/v1/infrastructure/outputs.tf
@@ -1,13 +1,5 @@
-output "cluster_name" {
-  value = module.eks_blueprints.eks_cluster_id
-}
-
 output "cluster_region" {
   value = var.environment.inputs.aws_region
-}
-
-output "configure_kubectl" {
-  value = module.eks_blueprints.configure_kubectl
 }
 
 output "vpc_id" {
@@ -15,11 +7,11 @@ output "vpc_id" {
 }
 
 output "public_subnets" {
-  value = module.vpc.public_subnets
+  value = jsonencode(join(",", module.vpc.public_subnets))
 }
 
 output "private_subnets" {
-  value = module.vpc.private_subnets
+  value = jsonencode(join(",", module.vpc.private_subnets))
 }
 
 output "eks_cluster_id" {
