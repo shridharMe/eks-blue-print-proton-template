b262b0c9423e24f2e050e90499b377190951344b
Shridhar Patil <saspatil@amazon.co.uk>
2022-09-23 14:07:23 +0100

    set demo

--- service-template/templates/micro-service1/v1/instance_infrastructure/versions.tf
+++ service-template/templates/micro-service1/v1/instance_infrastructure/versions.tf
@@ -12,36 +12,24 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.10"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.4.1"
-    }
   }
   required_version = "~>1.0"
 }
 
 provider "aws" {
-  region = var.environment.inputs.aws_region
+  region = var.environment.outputs.cluster_region
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks_blueprints.eks_cluster_id
+  name = var.environment.outputs.eks_cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks_blueprints.eks_cluster_id
+  name = var.environment.outputs.eks_cluster_id
 }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
 }
---
+++ service-template/templates/micro-service3/v1/instance_infrastructure/versions.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.14.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~>2.2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+  }
+  required_version = "~>1.0"
+}
+
+provider "aws" {
+  region = var.environment.outputs.cluster_region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = var.environment.outputs.eks_cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.environment.outputs.eks_cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
